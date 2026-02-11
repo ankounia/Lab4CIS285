21bf6d302d48fe39b717f7e6f705e4ad7a69ecb8
ankounia <your-email@example.com>
2026-02-10 20:40:59 -0500

    Jenkins build

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 public static void main(String[] args) {
 for(int i=1;i<=10;i++) {
-System.out.println("Hello CIS285..."+i);
+System.out.println("Hello Jenkins..."+i);
 }
 }
 }
