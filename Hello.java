41f3ad0ed3fee3913637f192652588caca7b8700
ankounia <your-email@example.com>
2026-02-10 20:58:41 -0500

    Trigger Jenkins build 2

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 public static void main(String[] args) {
 for(int i=1;i<=10;i++) {
-System.out.println("Hello Jenkins build..."+i);
+System.out.println("Hello Jenkins new build..."+i);
 }
 }
 }
